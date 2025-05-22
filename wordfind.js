52cd68c8df41f87aa2748905e4a56f409465bed8
ThetaXi-Spanky <60678453+ThetaXi-Spanky@users.noreply.github.com>
2025-05-22 12:07:07 -0400

    Add files via upload

---
+++ wordfind.js
@@ -0,0 +1,23 @@
+/**
+* Wordfind.js 0.0.1
+* (c) 2012 Bill, BunKat LLC.
+* Wordfind is freely distributable under the MIT license.
+* For all details and documentation:
+*     http://github.com/bunkat/wordfind
+*/
+
+(function () {
+
+  'use strict';
+
+  /**
+  * Generates a new word find (word search) puzzle provided a set of words.
+  * Can automatically determine the smallest puzzle size in which all words
+  * fit, or the puzzle size can be manually configured.  Will automatically
+  * increase puzzle size until a valid puzzle is found.
+  *
+  * WordFind has no dependencies.
+  */
+
+  /**
+  *
---
+++ wordfindgame.js
@@ -0,0 +1,103 @@
+/**
+* Wordfind.js 0.0.1
+* (c) 2012 Bill, BunKat LLC.
+* Wordfind is freely distributable under the MIT license.
+* For all details and documentation:
+*     http://github.com/bunkat/wordfind
+*/
+
+(function (document, $, wordfind) {
+  'use strict';
+
+  /**
+  * An example game using the puzzles created from wordfind.js. Click and drag
+  * to highlight words.
+  *
+  * WordFindGame requires wordfind.js and jQuery.
+  */
+
+    /**
+    * Draws the puzzle by inserting rows of buttons into el.
+    *
+    * @param {String} el: The jQuery element to write the puzzle to
+    * @param {[[String]]} puzzle: The puzzle to draw
+    */
+    var drawPuzzle = function (el, puzzle) {
+      var output = '';
+      // for each row in the puzzle
+      for (var i = 0, height = puzzle.length; i < height; i++) {
+        // append a div to represent a row in the puzzle
+        var row = puzzle[i];
+        output += '<div>';
+        // for each element in that row
+        for (var j = 0, width = row.length; j < width; j++) {
+            // append our button with the appropriate class
+            output += '<button class="puzzleSquare" x="' + j + '" y="' + i + '">';
+            output += row[j] || '&nbsp;';
+            output += '</button>';
+        }
+        // close our div that represents a row
+        output += '</div>';
+      }
+
+      $(el).html(output);
+    };
+
+    var getWords = function () {
+      return $('input.word').toArray().map(wordEl => wordEl.value.toLowerCase()).filter(word => word);
+    };
+
+    /**
+    * Given two points, ensure that they are adjacent and determine what
+    * orientation the second point is relative to the first
+    *
+    * @param {int} x1: The x coordinate of the first point
+    * @param {int} y1: The y coordinate of the first point
+    * @param {int} x2: The x coordinate of the second point
+    * @param {int} y2: The y coordinate of the second point
+    */
+    var calcOrientation = function (x1, y1, x2, y2) {
+
+      for (var orientation in wordfind.orientations) {
+        var nextFn = wordfind.orientations[orientation];
+        var nextPos = nextFn(x1, y1, 1);
+
+        if (nextPos.x === x2 && nextPos.y === y2) {
+          return orientation;
+        }
+      }
+
+      return null;
+    };
+
+
+  /**
+  * Initializes the WordFindGame object.
+  *
+  * Creates a new word find game and draws the board and words.
+  *
+  * Returns the puzzle that was created.
+  *
+  * @param {String} puzzleEl: Selector to use when inserting the puzzle
+  * @param {Options} options: WordFind options to use when creating the puzzle
+  */
+  var WordFindGame = function (puzzleEl, options) {
+
+    // Class properties, game initial config:
+    var wordList, puzzle;
+
+    /**
+    * Game play events.
+    *
+    * The following events handle the turns, word selection, word finding, and
+    * game end.
+    *
+    */
+
+    // Game state
+    var startSquare, selectedSquares = [], curOrientation, curWord = '';
+
+    /**
+    * (game implementation continues...)
+    */
+})();
